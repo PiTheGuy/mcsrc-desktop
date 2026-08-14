c5a5e2b83029f0cdbb288d6635ede13d6f72d64c
PiTheGuy <55959311+PiTheGuy@users.noreply.github.com>
2026-08-14 17:33:05 -0500

    Version bump

--- src/main/java/pitheguy/mcsrcdesktop/util/SharedConstants.java
+++ src/main/java/pitheguy/mcsrcdesktop/util/SharedConstants.java
@@ -2,5 +2,5 @@ package pitheguy.mcsrcdesktop.util;
 
 public class SharedConstants {
     public static final int PROTOCOL_VERSION = 1;
-    public static final String APP_VERSION = "1.0.0";
+    public static final String APP_VERSION = "1.0.1";
 }
